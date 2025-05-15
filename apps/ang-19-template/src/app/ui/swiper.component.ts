53a0f1a43671ae5f944594caf230265c4df6f112
xcode1880 <xcode.mac.1980>
2025-05-15 09:49:27 +0300

    [FEAT] : fix styles
    * fix styles

--- apps/ang-19-template/src/app/ui/swiper.component.ts
+++ apps/ang-19-template/src/app/ui/swiper.component.ts
@@ -3,15 +3,22 @@ import {
   Component,
   CUSTOM_ELEMENTS_SCHEMA,
   Input,
+  OnDestroy,
+  signal,
 } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Carousel } from 'primeng/carousel';
 import { Button } from 'primeng/button';
+import { Subscription, timer } from 'rxjs';
+
+const TIMER_DURATION = 20000; // 20 seconds in milliseconds
 
 interface DecodedQuestion {
   question: string;
   correctAnswer: string;
   incorrectAnswers: string[];
+  selectedAnswer?: string;
+  disabled?: boolean;
 }
 
 @Component({
@@ -24,10 +31,64 @@ interface DecodedQuestion {
   host: { ngSkipHydration: 'true' },
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SwiperComponent {
+export class SwiperComponent implements OnDestroy {
   @Input() results: DecodedQuestion[] = [];
+  isCorrect = signal(false);
+  isSelected = false;
+  timeLeft = signal(20);
+  timerDuration = TIMER_DURATION;
+  #timerSubscription?: Subscription;
+
+  constructor() {
+    this.startTimer();
+  }
+
+  startTimer() {
+    this.timeLeft.set(20);
+    this.#timerSubscription?.unsubscribe();
+    this.#timerSubscription = timer(0, 1000).subscribe(() => {
+      if (this.timeLeft() > 0) {
+        this.timeLeft.update((value) => value - 1);
+      } else {
+        this.#timerSubscription?.unsubscribe();
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    this.#timerSubscription?.unsubscribe();
+  }
 
   getAllAnswers(question: DecodedQuestion): string[] {
     return [question.correctAnswer, ...question.incorrectAnswers];
+  }
+
+  handleAnswer(question: DecodedQuestion, answer: string) {
+    if (question.disabled) return;
+
+    question.selectedAnswer = answer;
+    question.disabled = true;
+    this.isSelected = true;
+    this.getAnswer(question, answer);
+    this.#timerSubscription?.unsubscribe();
+  }
+
+  getAnswer(question: DecodedQuestion, answer: string) {
+    if (answer === question.correctAnswer) {
+      this.isCorrect.set(true);
+    }
+
+    if (
+      answer === question.selectedAnswer &&
+      answer !== question.correctAnswer
+    ) {
+      this.isCorrect.set(false);
+    }
+  }
+
+  onPageChange() {
+    this.isCorrect.set(false);
+    this.isSelected = false;
+    this.startTimer();
   }
 }
